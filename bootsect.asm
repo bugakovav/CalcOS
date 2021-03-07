c7439f44ff20c481ffb50ad34e991a05d80e60b2
Bugakov Artem <bugakov.data@gmail.com>
2021-03-07 23:11:02 +0300

    ChangeColorInBootsectAndFuncCalc

--- bootsect.asm
+++ bootsect.asm
@@ -1,54 +1,77 @@
-use16
-org 0x7C00
-start:
-	mov ax, cs
-	mov ds, ax
-	mov ss, ax 
-	mov sp, start 
+.code16
+.org 0x7c00
+start: 
+        movw %cs,%ax
+        movw %ax,%ds
+        movw %ax,%ss
+        movw $start, %sp
 
-	mov ah, 0x00
-	mov al, 0x03
-	int 0x10
+        movb $0x00,%ah
+        movb $0x03,%al
+        int $0x10
 
-load_drive:
-    mov bx, 0x1000
-    mov es, bx
-    xor bx, bx
-    mov al, 0x30
-    mov dl, 0x01 
-    mov dh, 0x00 
-    mov ch, 0x00 
-    mov cl, 0x01 
-    mov ah, 0x02 
-    int 0x13 
+wait_input:
+        mov $0x00, %ah
+        int $0x16
+        cmpb $0x31, %al
+        mov $0x31, 0x7e00
+        je load_drive
+        cmpb $0x32, %al
+        mov $0x32, 0x7e00
+        je load_drive
+        cmpb $0x33, %al
+        mov $0x33, 0x7e00
+        je load_drive
+        cmpb $0x34, %al
+        mov $0x34, 0x7e00
+        je load_drive
+        cmpb $0x35, %al
+        mov $0x35, 0x7e00
+        je load_drive
+        cmpb $0x36, %al
+        mov $0x36, 0x7e00
+        je load_drive
+        jmp wait_input
 
-enable_protected_mode:
-	cli
-	lgdt[gdt_info]
-	in al, 0x92
-	or al, 2
-	out 0x92, al
-	mov eax, cr0
-	or al, 1
-	mov cr0, eax
-	jmp 0x8:protected_mode 
+load_drive: 
+    movw $0x1000,%bx
+    movw %bx,%es
+    xorw %bx,%bx
+    movb $0x30,%al
+    movb $0x01,%dl
+    movb $0x00,%dh
+    movb $0x00,%ch
+    movb $0x01,%cl
+    movb $0x02,%ah
+    int $0x13
 
-gdt:
-	db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-	db 0xff, 0xff, 0x00, 0x00, 0x00, 0x9A, 0xCF, 0x00 
-	db 0xff, 0xff, 0x00, 0x00, 0x00, 0x92, 0xCF, 0x00
+enable_protected_mode: 
+        cli
+        lgdt gdt_info   
+        inb $0x92,%al
+        orb $2,%al
+        outb %al, $0x92
+        movl %cr0, %eax
+        orb $1,%al
+        movl %eax, %cr0
+        ljmp $0x8, $protected_mode
+
+gdt: 
+        .byte 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+        .byte 0xff,0xff,0x00,0x00,0x00,0x9A,0xCF,0x00
+        .byte 0xff,0xff,0x00,0x00,0x00,0x92,0xCF,0x00
 
 gdt_info: 
-	dw gdt_info - gdt
-	dw gdt, 0
+    .word gdt_info - gdt 
+    .word gdt, 0
 
-use32
-protected_mode:
-	mov ax, 0x10 
-	mov es, ax
-	mov ds, ax
-	mov ss, ax
-	call 0x10000 
+.code32
+protected_mode: 
+        movw $0x10,%ax
+        movw %ax,%es
+        movw %ax,%ds
+        movw %ax,%ss
+        call 0x10000
 
-times (512 -($ - $$) - 2) db 0
-db 0x55, 0xAA 
+.zero (512 - (. - start) - 2)
+.byte 0x55, 0xAA
--- kernel.cpp
+++ kernel.cpp
@@ -5,6 +5,7 @@ __asm("jmp kmain");
 #define VIDEO_BUF_PTR (0xb8000)
 #define IDT_TYPE_INTR (0x0E)
 #define IDT_TYPE_TRAP (0x0F)
+#define SYSTEM_COLOR (0x7e00)
 
 #define GDT_CS (0x8)
 #define PIC1_PORT (0x20)
@@ -12,7 +13,6 @@ __asm("jmp kmain");
 #define VIDEO_WIDTH (80)
 
 #define DEFAULT_COLOR 0x0F
-#define SIZE_BUF 40
 
 #define FAIL_EXIT -1
 #define SUCCESS_EXIT 0
@@ -34,6 +34,11 @@ __asm("jmp kmain");
 
 #define INT_MAX 2147483647
 #define INT_MIN -2147483648
+
+#define BUF_SIZE 40
+
+#define SYSTEM_NL 0
+#define USER_NL 1
 
 typedef unsigned short u_short;
 typedef unsigned int u_int;
@@ -71,8 +76,11 @@ u_short g_color;
 u_short g_strnum;
 u_short g_pos;
 
-u_char g_buf[256];
+u_char g_buf[BUF_SIZE];
 u_short g_buf_pos;
+
+u_char g_expr[BUF_SIZE];
+u_short g_expr_pos;
 
 enum cmd_n
 {
@@ -86,12 +94,15 @@ enum cmd_n
 
 struct info_t
 {
+    c_char *hello;
     c_char *name_os;
     c_char *name_comp;
     c_char *info_op;
     c_char *error_cmd;
     c_char *help;
 } os_info;
+
+u_char available[] = {"1234567890+/-*\0"};
 
 char scan_codes[] =
     {
@@ -109,7 +120,7 @@ char scan_codes[] =
         0,                            // right SHIFT(54)
         '*',                          // NUMPAD *
         0,                            // ALT
-        ' ',                          // SPACE
+        0,                            // SPACE
         0,                            //CAPSLOCK
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //F1 - F10
         0,                            //NUMLOCK
@@ -129,21 +140,21 @@ void intr_init();
 void intr_start();
 void intr_enable();
 void intr_disable();
-void out_str(int color, const char *ptr, unsigned int strnum);
 
-void out_char(int color, unsigned char simbol);
-void out_word(int color, const char *ptr);
-void out_num(int num);
 static inline void outw(unsigned int port, unsigned int data);
-
 static inline unsigned char inb(unsigned short port);             // Чтение из порта
 static inline void outb(unsigned short port, unsigned char data); // Запись
+
 void keyb_init();
 void keyb_handler();
 void keyb_process_keys();
 void cursor_moveto(unsigned int strnum, unsigned int pos);
 
-bool strcmp(char *str, char *cmd);
+void write_char(u_char s);
+void write_str(c_char *str);
+
+bool strcmp_buf(u_char *s1, c_char *s2);
+void clear_buf(u_char *str, u_int size);
 
 void mv_cur_r(void);
 void mv_cur_l(void);
@@ -151,22 +162,59 @@ void mv_cur_nl(bool user);
 
 int read_command(void);
 void handle_command(int num);
+int handle_expr(void);
 
 bool init()
 {
     intr_disable();
 
-    os_info.name_os = "Welcome to CalcOS\0";
-    os_info.name_comp = "YASM & GCC\0";
-    os_info.info_op = "The loader has a global color selection function\0";
-    os_info.error_cmd = "Incorrect command\0";
-    os_info.help = "Commads: info, expr, shutdown\0";
+    switch (*(char *)SYSTEM_COLOR)
+    {
+    case '1': // green
+        g_color = 0x02;
+        break;
+    case '2': // blue
+        g_color = 0x01;
+        break;
+    case '3': // red
+        g_color = 0x04;
+        break;
+    case '4': // yellow
+        g_color = 0x0E;
+        break;
+    case '5': // gray
+        g_color = 0x07;
+        break;
+    case '6': // white
+        g_color = 0x0F;
+        break;
+    default:
+        g_color = DEFAULT_COLOR;
+        break;
+    }
 
-    g_color = DEFAULT_COLOR;
     g_strnum = 0;
     g_pos = 0;
 
+    os_info.hello = "Welcome CalcOS!\0";
+
+    write_str(os_info.hello);
+    mv_cur_nl(SYSTEM_NL);
+
+    os_info.name_os = "Calc OS: v.01. Developer: Bugakov Artem, 54/1, SpbPU, 2021\0";
+    os_info.name_comp = "Compilers: bootloader: YASM, kernel: gcc\0";
+
+    os_info.info_op = "Bootloader parameters: green color.\0";
+
+    os_info.error_cmd = "Error: command not recognized\0";
+
+    os_info.help = "Not implemented\0";
+
     g_buf_pos = 0;
+    clear_buf(g_buf, BUF_SIZE);
+
+    g_expr_pos = 0;
+    clear_buf(g_expr, BUF_SIZE);
 
     intr_init();
     keyb_init();
@@ -176,15 +224,15 @@ bool init()
     return true;
 }
 
-bool strcmp(char *str, char *cmd)
+bool strcmp_buf(u_char *s1, c_char *s2)
 {
-    while (*str AND * cmd AND * str == *cmd AND * str != ' ')
+    while (*s1 AND * s2 AND * s1 == *s2)
     {
-        str += 1;
-        cmd += 1;
+        s1 += 1;
+        s2 += 1;
     }
 
-    if (*str != *cmd)
+    if (*s1 != *s2)
         return false;
     else
         return true;
@@ -218,6 +266,25 @@ void write_char(u_char s)
     video_buf += 2;
 }
 
+void clear_buf(u_char *str, u_int size)
+{
+    for (int i = 0; i < size; i++)
+        str[i] = '\0';
+}
+
+u_int strlen(c_char *str)
+{
+    int i;
+
+    for (i = 0; str[i] != '\0' and i < BUF_SIZE; i++)
+        ;
+
+    if (i = BUF_SIZE)
+        return 0;
+    else
+        return i;
+}
+
 void on_key(u_char scan_code) // Обработчик клавиши
 {
     u_int index = (u_int)scan_code; // Индексация по таблице
@@ -227,15 +294,15 @@ void on_key(u_char scan_code) // Обработчик клавиши
     if (index == L_SHIFT OR index == R_SHIFT) // Спец. клавиши
         return;
 
-    if (index != BACKSPACE) // Добавление в программный буфер
-        g_buf[g_buf_pos] = sym;
+    if (index != BACKSPACE and index != ENTER) // Добавление в программный буфер
+        g_buf[g_buf_pos++] = sym;
 
     write_char(sym); // Вывод символа
 
     switch (index) // Проверка индекса на спец. клавиши
     {
     case ENTER:
-        mv_cur_nl(TRUE);
+        mv_cur_nl(USER_NL);
         break;
     case BACKSPACE:
         mv_cur_l();
@@ -248,12 +315,10 @@ void on_key(u_char scan_code) // Обработчик клавиши
 
 void mv_cur_r(void)
 {
-    if (g_pos + 1 >= VIDEO_WIDTH - 40)
+    if (g_pos + 1 > VIDEO_WIDTH)
         return;
 
     g_pos += 1;
-    g_buf_pos += 1;
-
     cursor_moveto(g_strnum, g_pos);
 }
 
@@ -285,45 +350,83 @@ void mv_cur_nl(bool user) // NEW LINE
 
 int read_command(void)
 {
-    if (strcmp((char *)g_buf, (char *)"help"))
-        return help;
-    if (strcmp((char *)g_buf, (char *)"info"))
+    if (strcmp_buf(g_buf, "info"))
         return info;
-    if (strcmp((char *)g_buf, (char *)"expr"))
+    if (strcmp_buf(g_buf, "expr"))
         return expr;
 
     return wrong_cmd;
+}
+
+void info_func(void)
+{
+    write_str(os_info.name_os);
+    mv_cur_nl(SYSTEM_NL);
+    write_str(os_info.name_comp);
+    mv_cur_nl(SYSTEM_NL);
+    write_str(os_info.info_op);
+    mv_cur_nl(SYSTEM_NL);
+}
+
+void invalid_func(void)
+{
+    write_str(os_info.error_cmd);
+    mv_cur_nl(SYSTEM_NL);
+}
+
+bool is_avilable(u_char sym)
+{
+    for (int i = 0; i < 16; i++)
+        if (sym == available[i])
+            return true;
+
+    return false;
+}
+
+bool validate_expr_arg(void)
+{
+    for (int i = 5; i < BUF_SIZE; i++) // strlen("expr\0");
+        if (!is_avilable(g_buf[i]))
+            return false;
+
+    return true;
+}
+
+void expr_func(void)
+{
+    if (!validate_expr_arg())
+    {
+        write_str("Incorrect argument\0");
+        mv_cur_nl(SYSTEM_NL);
+        return;
+    }
+
+    for (int i = 5; i < BUF_SIZE; i++) // write expr to buf for func
+        if (g_buf[i] != '\0')
+            g_expr[g_expr_pos++] = g_buf[i];
+
+    handle_expr();
+
+    clear_buf(g_expr, BUF_SIZE);
+    g_expr_pos = 0;
 }
 
 void handle_command(int num)
 {
     switch (num)
     {
-    case help:
-        write_str(os_info.help);
-        mv_cur_nl(FALSE);
-        break;
     case info:
-        write_str(os_info.name_os);
-        mv_cur_nl(FALSE);
-        write_str(os_info.name_comp);
-        mv_cur_nl(FALSE);
-        write_str(os_info.info_op);
-        mv_cur_nl(FALSE);
+        info_func();
         break;
     case expr:
-        write_str("Hello from expr");
-        mv_cur_nl(FALSE);
+        expr_func();
         break;
     case wrong_cmd:
-        write_str(os_info.error_cmd);
-        mv_cur_nl(FALSE);
+        invalid_func();
         break;
     }
 
-    for (int i = 0; i < SIZE_BUF; i++) // Очистить программный буфер
-        g_buf[i] = '\0';
-
+    clear_buf(g_buf, BUF_SIZE);
     g_buf_pos = 0;
 }
 
@@ -463,4 +566,414 @@ void cursor_moveto(unsigned int strnum, unsigned int pos)
     outb(CURSOR_PORT + 1, (unsigned char)(new_pos & 0xFF));
     outb(CURSOR_PORT, 0x0E);
     outb(CURSOR_PORT + 1, (unsigned char)((new_pos >> 8) & 0xFF));
+}
+
+int atoi(char *str)
+{
+    int sign = 1, base = 0, i = 0;
+
+    while (str[i] == ' ')
+    {
+        i++;
+    }
+
+    if (str[i] == '-' || str[i] == '+')
+    {
+        sign = 1 - 2 * (str[i++] == '-');
+    }
+
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        if (base > INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7))
+        {
+            if (sign == 1)
+                return INT_MAX;
+            else
+                return INT_MIN;
+        }
+        base = 10 * base + (str[i++] - '0');
+    }
+    return base * sign;
+}
+
+char *itoa(int num, char *str, int base)
+{
+    int i = 0;
+    bool isNegative = false;
+
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+
+    if (num < 0 && base == 10)
+    {
+        isNegative = true;
+        num = -num;
+    }
+
+    while (num != 0)
+    {
+        int rem = num % base;
+        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+        num = num / base;
+    }
+
+    if (isNegative)
+        str[i++] = '-';
+
+    str[i] = '\0';
+
+    int start = 0;
+    int end = i - 1;
+    char tmp;
+
+    while (start < end)
+    {
+        tmp = *(str + start);
+        *(str + start) = *(str + end);
+        *(str + end) = tmp;
+
+        start++;
+        end--;
+    }
+
+    return str;
+}
+
+#define SIZE_BUF 40
+
+//char available[] = {"1234567890-+/*\0"};
+char digits[] = {"1234567890\0"};
+
+int p_input = 0;
+char input[SIZE_BUF];
+
+bool isdigit(char sym)
+{
+    for (int i = 0; i < strlen(digits); i++)
+    {
+        if (digits[i] == sym)
+            return true;
+    }
+
+    return false;
+}
+
+char limit_max[] = {"2147483647"};
+char limit_min[] = {"-2147483648"};
+
+bool validate_int_max(char *str)
+{
+    if (strlen(str) > strlen(limit_max))
+        return false;
+    else if (strlen(str) == strlen(limit_max))
+    {
+        for (int i = 0; i < strlen(str); i++)
+        {
+            if (str[i] - '0' < limit_max[i] - '0')
+                return true;
+            if (str[i] - '0' > limit_max[i] - '0')
+                return false;
+        }
+    }
+    else
+        return true;
+
+    return true;
+}
+
+bool validate_int_min(char *str)
+{
+    if (strlen(str) > strlen(limit_min))
+        return false;
+    else if (strlen(str) == strlen(limit_min))
+    {
+        for (int i = 1; i < strlen(str); i++)
+        {
+            if (str[i] - '0' < limit_min[i] - '0')
+                return true;
+            if (str[i] - '0' > limit_min[i] - '0')
+                return false;
+        }
+    }
+    else
+        return true;
+
+    return true;
+}
+
+bool validate_num(char *num)
+{
+    bool success;
+
+    if (num[0] == '-')
+        success = validate_int_min(num);
+    else
+        success = validate_int_max(num);
+
+    return success;
+}
+
+struct elem_output
+{
+    char op;
+    int num;
+    bool select;
+};
+
+typedef struct elem_output elem_output;
+
+elem_output output[256]; // Массив для ОПЗ
+
+char stack[256]; // Стек для операций
+
+int int_stack[256]; // Стек для результирующей функции calc
+
+int p_stack = 0, p_output = 0;
+
+void clean_str(char *str)
+{
+    for (int i = 0; i < strlen(str) and i < SIZE_BUF; i++)
+        str[i] = 0;
+}
+
+int RESULT = -1;
+
+bool calc(void)
+{
+    p_stack = 0;
+
+    for (int i = 0; i < p_output; i++)
+    {
+        switch (output[i].select)
+        {
+        case 0:
+            int_stack[p_stack++] = output[i].num;
+            break;
+        case 1:
+        {
+            int op1, op2;
+
+            int no_overflow = 1;
+
+            if (p_stack >= 1) // Для бинарной операции должно быть точно два операнда
+            {
+                op2 = int_stack[p_stack - 1];
+                op1 = int_stack[p_stack - 2];
+
+                p_stack -= 2;
+
+                switch (output[i].op)
+                {
+                case '+':
+                    int_stack[p_stack++] = op1 + op2;
+                    asm("jno NO_OVERFLOW_SUM");
+                    asm("mov $0x0, %0"
+                        : "=r"(no_overflow));
+                    asm("NO_OVERFLOW_SUM:");
+                    break;
+                case '-':
+                    int_stack[p_stack++] = op1 - op2;
+                    asm("jno NO_OVERFLOW_SUB");
+                    asm("mov $0x0, %0"
+                        : "=r"(no_overflow));
+                    asm("NO_OVERFLOW_SUB:");
+                    break;
+                case '*':
+                    int_stack[p_stack++] = op1 * op2;
+                    asm("jno NO_OVERFLOW_MULT");
+                    asm("mov $0x0, %0"
+                        : "=r"(no_overflow));
+                    asm("NO_OVERFLOW_MULT:");
+                    break;
+                case '/':
+                    if (op2 == 0)
+                    {
+                        write_str("Error: division by 0");
+                        mv_cur_nl(SYSTEM_NL);
+                        return false;
+                    }
+                    int_stack[p_stack++] = op1 / op2;
+                    asm("jno NO_OVERFLOW_DIV");
+                    asm("mov $0x0, %0"
+                        : "=r"(no_overflow));
+                    asm("NO_OVERFLOW_DIV:");
+                    break;
+                }
+
+                if (!no_overflow)
+                {
+                    write_str("Error: integer overflow");
+                    mv_cur_nl(SYSTEM_NL);
+
+                    return false;
+                }
+            }
+            else
+            {
+                write_str("Error: ivalid expression");
+                mv_cur_nl(SYSTEM_NL);
+
+                return false;
+            }
+            break;
+        }
+        }
+    }
+
+    RESULT = int_stack[0];
+
+    char res[SIZE_BUF] = {0};
+
+    itoa(RESULT, res, 10);
+
+    write_str((c_char *)res);
+    mv_cur_nl(SYSTEM_NL);
+
+    return true;
+}
+
+int handle_expr(void)
+{
+    int num_int = 0;
+    char num_char[SIZE_BUF];
+    int p_num = 0;
+
+    for (int i = 0; i < strlen((c_char *)g_expr); i++)
+    {
+        if (isdigit(g_expr[i]))
+        {
+            while (isdigit(g_expr[i]))
+            {
+                num_char[p_num++] = g_expr[i++];
+            }
+
+            p_num = 0;
+            i--;
+
+            if (!validate_num(num_char))
+            {
+                write_str("Error: integer overflow");
+                mv_cur_nl(SYSTEM_NL);
+
+                return -1;
+            }
+
+            num_int = atoi(num_char);
+
+            output[p_output].select = 0; // 0 - num, 1 - op
+            output[p_output++].num = num_int;
+
+            clean_str(num_char);
+            continue;
+        }
+
+        switch (g_expr[i])
+        {
+        case '+':
+        {
+            while (i < SIZE_BUF - 1 and g_expr[i + 1] == '+')
+                i++;
+
+            if (i < SIZE_BUF - 1 and g_expr[i + 1] != '-' and g_expr[i + 1] != '+' and !isdigit(g_expr[i + 1]))
+            {
+                write_str("Next value is not digit or '('");
+                mv_cur_nl(SYSTEM_NL);
+
+                return -1;
+            }
+
+            while (p_stack > 0 and stack[p_stack - 1] == '*' or stack[p_stack - 1] == '/')
+            {
+                output[p_output].select = 1;
+                output[p_output++].op = stack[p_stack - 1];
+                p_stack--;
+            }
+            stack[p_stack++] = '+';
+            break;
+        }
+        case '-':
+        {
+            int count_minus = 1; // Для подсчета унарных минусов
+            while (i < SIZE_BUF - 1 and g_expr[i + 1] == '-')
+            {
+                i++;
+                count_minus++;
+            }
+            if (i < SIZE_BUF - 1 and g_expr[i + 1] != '-' and g_expr[i + 1] != '+' and !isdigit(g_expr[i + 1]))
+            {
+                write_str("Next value is not digit or '('");
+                mv_cur_nl(SYSTEM_NL);
+
+                return -1;
+            }
+            while (p_stack > 0 and stack[p_stack - 1] == '*' or stack[p_stack - 1] == '/')
+            {
+                output[p_output].select = 1;
+                output[p_output++].op = stack[p_stack - 1];
+                p_stack--;
+            }
+            if (count_minus % 2 == 0)
+                stack[p_stack++] = '+'; // Зависит от количества унарных минусов
+            else
+                stack[p_stack++] = '-';
+            break;
+        }
+        case '*':
+        {
+            if (i < SIZE_BUF - 1 and g_expr[i + 1] != '-' and g_expr[i + 1] != '+' and !isdigit(g_expr[i + 1])) // Если после текущей операции стоит не число и не скобка
+            {
+                write_str("Next value is not digit or '('");
+                mv_cur_nl(SYSTEM_NL);
+
+                return -1;
+            }
+            while (p_stack > 0 and stack[p_stack - 1] == '*' or stack[p_stack - 1] == '/') // Если сверху стека операция приоритетнее текущей
+            {
+                output[p_output].select = 1;
+                output[p_output++].op = stack[p_stack - 1];
+                p_stack--;
+            }
+            stack[p_stack++] = '*';
+            break;
+        }
+        case '/':
+        {
+            if (i < SIZE_BUF - 1 and g_expr[i + 1] != '-' and g_expr[i + 1] != '+' and !isdigit(g_expr[i + 1]))
+            {
+                write_str("Next value is not digit or '('");
+                mv_cur_nl(SYSTEM_NL);
+
+                return -1;
+            }
+
+            while (p_stack > 0 and stack[p_stack - 1] == '*' or stack[p_stack - 1] == '/')
+            {
+                output[p_output].select = 1;
+                output[p_output++].op = stack[p_stack - 1];
+                p_stack--;
+            }
+            stack[p_stack++] = '/';
+            break;
+        }
+        default:
+            write_str("Error: invalid expression");
+            mv_cur_nl(SYSTEM_NL);
+
+            return -1;
+        }
+    }
+
+    for (int i = 0; i < p_stack; i++)
+    {
+        output[p_output].select = 1;
+        output[p_output++].op = stack[i];
+    }
+
+    calc();
+
+    return 0;
 }
